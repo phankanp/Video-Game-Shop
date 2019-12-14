babd7afd8c3cba2aebfa07e3ee2501d441bba346
Teja <phankanmp@gmail.com>
2019-12-14 15:30:26 -0800

    Implemented environment variables and added tests for pages and users apps

--- Video Game Shop V2/users/tests.py
+++ Video Game Shop V2/users/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse, resolve
 # Create your tests here.
 
 
@@ -18,7 +19,7 @@ class CustomUserTests(TestCase):
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
 
-    def test_create_superuser(self):
+    def test_create_adminuser(self):
         User = get_user_model()
         admin_user = User.objects.create_superuser(
             username='admin',
@@ -30,3 +31,29 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignupTests(TestCase):
+
+    username = 'testUser'
+    email = 'testUser@email.com'
+
+    def setUp(self):
+        url = reverse('account_signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'account/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(
+            self.response, 'Error!')
+
+    def test_signup_form(self):
+        new_user = get_user_model().objects.create_user(
+            self.username, self.email)
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()
+                         [0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()
+                         [0].email, self.email)
