71a71e0b0eb219219b48abe7abc78cc1e6d4f8c8
Teja <phankanmp@gmail.com>
2020-02-16 19:05:36 -0800

    redesigned home page template

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView, ListView
 
+from games.models import Game
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'pages/index.html')
+class HomePageView(ListView):
+    def get(self, request, *args, **kwargs):
+        games = Game.objects.all()
+
+        games_to_show = 0
+
+        if (len(games) < 9):
+            games_to_show = len(games)
+        else:
+            games_to_show = 9
+
+        homepage_games_list = []
+
+        for i in range(games_to_show):
+            homepage_games_list.append(games[i])
+
+        context = {
+            'games': homepage_games_list
+        }
+
+        return render(self.request, 'pages/index.html', context)
